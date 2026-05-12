a6d38d8546a5482c355cd72c20fe73c61c714f91
Shekinah Jubahib <shekinahjubahib19@gmail.com>
2026-05-12 09:34:53 +0800

    Enhance button styles for edit and delete actions in asset groups table

--- public/assets/js/asset-groups.js
+++ public/assets/js/asset-groups.js
@@ -122,10 +122,10 @@ document.addEventListener('DOMContentLoaded', function() {
                         <td class="px-6 py-0 text-sm font-medium text-slate-700 text-center whitespace-nowrap">${escapeHtml(r.asset_gl_code || '')}</td>
                         <td class="px-6 py-0 text-sm font-medium text-slate-700 text-center whitespace-nowrap">${escapeHtml(r.expense_gl_code || '')}</td>
                         <td class="px-6 py-0 text-center text-sm">
-                            <button data-id="${r.id}" class="edit-btn inline-flex items-center justify-center w-6 h-6 bg-white hover:bg-slate-50 text-slate-700 rounded-sm shadow-sm transition" title="Edit">
+                            <button data-id="${r.id}" class="edit-btn inline-flex items-center justify-center w-6 h-6 bg-white hover:bg-slate-50 hover:text-red-600  text-slate-400 rounded-sm shadow-sm transition" title="Edit">
                                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5M18.5 2.5a2.121 2.121 0 113 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
                             </button>
-                            <button data-id="${r.id}" class="delete-btn inline-flex items-center justify-center w-6 h-6 ml-1 bg-white hover:bg-red-50 text-red-600 rounded-sm shadow-sm transition" title="Delete">
+                            <button data-id="${r.id}" class="delete-btn inline-flex items-center justify-center w-6 h-6 ml-1 bg-white hover:bg-red-50 hover:text-red-600 text-slate-400 rounded-sm shadow-sm transition" title="Delete">
                                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M9 7V4a1 1 0 011-1h4a1 1 0 011 1v3m-7 0h8"/></svg>
                             </button>
                         </td>
